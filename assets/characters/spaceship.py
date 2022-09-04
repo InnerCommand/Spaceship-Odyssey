9e0d8b166501125ec38c906ff390f4138c14794d
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-09-04 11:42:57 +0800

    added movements

--- assets/characters/spaceship.py
+++ assets/characters/spaceship.py
@@ -1,16 +1,29 @@
 import pygame
 
-class spaceship:
+class spaceship(pygame.Surface):
 	def __init__(self, surface : pygame.Surface, color : tuple[int], screenWidth, screenHeight) -> None:
 		# self.image = image
 		self.surface = surface
 		self.color = color
 		self.x = (screenWidth/2)-30
-		self.y = (screenHeight/2)-30
+		self.y = (screenHeight-50)-30
+		self.screenWidth = screenWidth
+		self.screenHeight = screenHeight
 
 	def draw(self) -> None:
-		pygame.draw.rect(self.surface, self.color, pygame.Rect(self.x,self.y,30,30))
+		rect = pygame.draw.rect(self.surface, self.color, pygame.Rect(self.x,self.y,30,30))
 
-	def moveForwards(self) -> None:
-		self.y -= 5 if self.y > 5 else 0
-		
+	def moveY(self, yAmt) -> None:
+		self.y += yAmt
+		if self.y > self.screenHeight-30:
+			self.y = self.screenHeight-30
+		elif self.y < self.screenHeight-150:
+			self.y = self.screenHeight-150
+	def moveX(self, xAmt) -> None:
+		self.x += xAmt
+		if self.x > self.screenWidth-30:
+			self.x = self.screenWidth-30
+		elif self.x < 0:
+			self.x = 0
+
+	
--- main.py
+++ main.py
@@ -14,19 +14,57 @@ player = spaceship(surface, (255,0,0), SCREENWIDTH, SCREENHEIGHT)
 
 running = True
 
-# Init Colors
+# Init Variables
 BLACK = (0, 0, 0)
+FPS = 30
+clock = pygame.time.Clock()
+
+moveState = {
+	'left' : False,
+	'right' : False,
+	'up' : False,
+	'down' : False
+}
 
 # Game loop
 while running:
+	# Updating frames
 	surface.fill(BLACK)
+	player.draw()
 
+	# Movements
+	if moveState['left'] == True:
+		player.moveX(-5)
+	if moveState['right'] == True:
+		player.moveX(5)
+	if moveState['up'] == True:
+		player.moveY(-5)
+	if moveState['down'] == True:
+		player.moveY(5)
+
+	# Check for keypress
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
-				player.moveForwards()
-				print('move')
+				moveState['up'] = True
+			if event.key == pygame.K_RIGHT:
+				moveState['right'] = True
+			if event.key == pygame.K_LEFT:
+				moveState['left'] = True
+			if event.key == pygame.K_DOWN:
+				moveState['down'] = True
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_UP:
+				moveState['up'] = False
+			if event.key == pygame.K_DOWN:
+				moveState['down'] = False
+			if event.key == pygame.K_LEFT:
+				moveState['left'] = False
+			if event.key == pygame.K_RIGHT:
+				moveState['right'] = False
 
-	player.draw()
+	# Updates
+	pygame.display.flip()
+	clock.tick(FPS)
