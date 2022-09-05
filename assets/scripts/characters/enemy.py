b0f1ca0c315cc99d704a5c384c742d49d8ef892d
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2022-09-05 15:25:30 +0800

    updated enemy script

--- assets/scripts/characters/enemy.py
+++ assets/scripts/characters/enemy.py
@@ -3,4 +3,22 @@ from assets.scripts.characters.spaceship import spaceship
 from assets.scripts.assets.bullet import bullet
 
 class enemy(spaceship):
-	pass
+	def __init__(self, surface : pygame.Surface, image : pygame.Surface, x : int, y : int, width : int, height : int, screenWidth : int, screenHeight : int) -> None:
+		"""
+		Set variables
+		"""
+		
+		# Set enemy details
+		self.image = pygame.transform.scale(image, (width, height))
+
+		# Set position of enemy
+		self.x = x
+		self.y = y
+
+		# Get details of pygame main board
+		self.surface = surface
+		self.screenWidth = screenWidth
+		self.screenHeight = screenHeight
+
+	def moveToPlayer(self, player : spaceship):
+		pass
