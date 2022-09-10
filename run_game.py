fcc228f7f32b1f1e135889da5eb8295f3387ff07
realhuman101 <78287030+realhuman101@users.noreply.github.com>
2022-09-10 16:45:30 +0800

    Update run_game.py

--- run_game.py
+++ run_game.py
@@ -5,7 +5,7 @@ MIN_VER = (3, 10)
 
 if sys.version_info[:2] < MIN_VER:
     sys.exit(
-        "This game requires Python {}.{}.".format(*MIN_VER)
+        "This game requires Python {}.{} or larger.".format(*MIN_VER)
     )
 
 # Run code
